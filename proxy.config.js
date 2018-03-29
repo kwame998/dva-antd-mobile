844a88746208b9ff8f669aab7038237ec1d9de25
kwame <kwame.huang@libratone.com.cn>
2018-03-29 23:13:12 +0800

    first commit

--- proxy.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '/api': {
-    //target:'http://bajiaoke.520igo.com',
-    target:'http://localhost:9003/',
-    changeOrigin: true,
-    "pathRewrite": { "^/api" : "" }
-  }
-};
